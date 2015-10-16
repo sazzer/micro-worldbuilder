795e3a2beacf2d226bd99710c8892f29ded2e726
Graham Cox <graham@grahamcox.co.uk>
2015-10-16 07:59:44 +0100

    Changed the Client Authntication Provider to be a pure AuthnticationProvider, meaning some of the Spring Security-ness of all that has gone

--- oauth2/webapp/src/main/kotlin/uk/co/grahamcox/worldbuilder/oauth2/client/Client.kt
+++ oauth2/webapp/src/main/kotlin/uk/co/grahamcox/worldbuilder/oauth2/client/Client.kt
@@ -11,46 +11,4 @@ import org.springframework.security.core.userdetails.UserDetails
  */
 data class Client(val id: ClientId,
                   val secret: ClientSecret,
-                  val grantedAuthorities: Collection<GrantedAuthority>) : UserDetails {
-    /**
-     * Get the Username of the Client. This is the Client ID
-     * @return the Username
-     */
-    override fun getUsername(): String = id.id
-
-    /**
-     * Get if the credentials are expired or not. For now this is always true
-     * @return true if the credentials are not expired.
-     */
-    override fun isCredentialsNonExpired(): Boolean = true
-
-    /**
-     * Get if the account is expired or not. For now this is always true
-     * @return true if the account is not expired.
-     */
-    override fun isAccountNonExpired(): Boolean = true
-
-    /**
-     * Get if the account is locked or not. For now this is always true
-     * @return true if the account is not locked.
-     */
-    override fun isAccountNonLocked(): Boolean = true
-
-    /**
-     * Get the Authorities of the client
-     * @return the authorities
-     */
-    override fun getAuthorities(): MutableCollection<out GrantedAuthority> = grantedAuthorities.toMutableSet()
-
-    /**
-     * Get if the client is enabled or not. For now this is always true
-     * @return true if the client is enabled
-     */
-    override fun isEnabled(): Boolean = true
-
-    /**
-     * Get the password of the account. For now this returns the hash, which is meaningless
-     * @return the hashed password
-     */
-    override fun getPassword(): String = secret.hash
-}
+                  val grantedAuthorities: Collection<GrantedAuthority>)
--- oauth2/webapp/src/main/kotlin/uk/co/grahamcox/worldbuilder/oauth2/client/ClientAuthenticationProvider.kt
+++ oauth2/webapp/src/main/kotlin/uk/co/grahamcox/worldbuilder/oauth2/client/ClientAuthenticationProvider.kt
@@ -1,49 +1,57 @@
 package uk.co.grahamcox.worldbuilder.oauth2.client
 
 import org.slf4j.LoggerFactory
+import org.springframework.security.authentication.AuthenticationProvider
 import org.springframework.security.authentication.BadCredentialsException
-import org.springframework.security.authentication.InternalAuthenticationServiceException
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
-import org.springframework.security.authentication.dao.AbstractUserDetailsAuthenticationProvider
-import org.springframework.security.core.authority.SimpleGrantedAuthority
-import org.springframework.security.core.userdetails.User
-import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.security.core.Authentication
 import org.springframework.security.core.userdetails.UsernameNotFoundException
 import uk.co.grahamcox.worldbuilder.oauth2.client.dao.ClientDao
 
 /**
  * Authentication Provider for loading Client details
  */
-class ClientAuthenticationProvider(private val clientDao: ClientDao) : AbstractUserDetailsAuthenticationProvider() {
+class ClientAuthenticationProvider(private val clientDao: ClientDao) : AuthenticationProvider {
     /** The logger to use */
     private val LOG = LoggerFactory.getLogger(ClientAuthenticationProvider::class.java)
 
     /**
-     * Retrieve the Client that has a ClientID of the given Username
-     * @param username The username to retrieve
-     * @param authentication The authentication token. Not used
-     * @return the client
+     * Actually authenticate the provided token. This must be a Username/Password Token, where the
+     * Username and Password are actually going to be treated as Client ID and Client Secret
+     * @param authentication The Authentication token
+     * @return the successful authentication
      */
-    override fun retrieveUser(username: String,
-                              authentication: UsernamePasswordAuthenticationToken): UserDetails =
-            clientDao.getById(ClientId(username)) ?: throw UsernameNotFoundException("Unknown client: ${username}")
+    override fun authenticate(authentication: Authentication): Authentication =
+        when(authentication) {
+            is UsernamePasswordAuthenticationToken -> {
+                val clientId = ClientId(authentication.principal.toString())
+                LOG.debug("Attempting to authenticate Client ID {}", clientId)
 
-    /**
-     * Actually check if the password we've got matches the password of the client
-     * @param userDetails The user details to compare
-     * @param authentication The authentication token to compare against
-     * @throws InternalAuthenticationServiceException if the user details are not a Client
-     * @throws BadCredentialsException if the Client Secret is wrong
-     */
-    override fun additionalAuthenticationChecks(userDetails: UserDetails,
-                                                authentication: UsernamePasswordAuthenticationToken) {
-        when (userDetails) {
-            is Client -> {
-                if (!userDetails.secret.compare(authentication.credentials.toString())) {
+                val client = clientDao.getById(clientId)
+                    ?: throw UsernameNotFoundException("Unknown client: ${clientId}")
+                LOG.debug("Retrieved client: {}", client)
+
+                if (!client.secret.compare(authentication.credentials.toString())) {
+                    LOG.debug("Client Secret was incorrect")
                     throw BadCredentialsException("Bad Password")
                 }
+                LOG.debug("Client Secret matched provided password")
+
+                val result = UsernamePasswordAuthenticationToken(authentication.principal,
+                    authentication.credentials,
+                    client.grantedAuthorities)
+                result.details = authentication.details
+                result
             }
-            else -> throw InternalAuthenticationServiceException("User Details were not of the correct type")
+            else -> throw IllegalArgumentException("Only UsernamePasswordAuthenticationToken is supported")
         }
-    }
+
+    /**
+     * Check if the Authentication Token that we have been provided is compatible with the one
+     * that we want to use
+     * @param authentication The type of Authentication Token we are going to be provided
+     * @return True if this is a Username/Password Token. False if not
+     */
+    override fun supports(authentication: Class<*>?) =
+        UsernamePasswordAuthenticationToken::class.java.isAssignableFrom(authentication)
 }
