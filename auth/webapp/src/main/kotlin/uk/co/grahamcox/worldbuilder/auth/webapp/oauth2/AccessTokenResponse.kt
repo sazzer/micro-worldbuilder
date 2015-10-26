8f39f38fc76106c818b66e52f14cf06cd273f1d5
Graham Cox <graham@grahamcox.co.uk>
2015-10-26 07:19:55 +0000

    Expose Scopes on a successful Access Token Response

--- auth/webapp/src/main/kotlin/uk/co/grahamcox/worldbuilder/auth/webapp/oauth2/AccessTokenResponse.kt
+++ auth/webapp/src/main/kotlin/uk/co/grahamcox/worldbuilder/auth/webapp/oauth2/AccessTokenResponse.kt
@@ -8,16 +8,19 @@ import com.fasterxml.jackson.annotation.JsonProperty
  * @param accessTokenValue The actual access token
  * @param tokenTypeValue The type of the token
  * @param expiresValue The time till expiry of the token
+ * @param scopesValue The scopes that the access token is valid for
  * @param refreshTokenValue The optional Refresh Token
  */
 @JsonInclude(JsonInclude.Include.NON_NULL)
 class AccessTokenResponse(accessTokenValue: String,
                                tokenTypeValue: String,
                                expiresValue: Int,
+                               scopesValue: String,
                                refreshTokenValue: String? = null) {
 
     @JsonProperty("access_token") var accessToken: String = accessTokenValue
     @JsonProperty("token_type") val tokenType: String = tokenTypeValue
     @JsonProperty("expires_in") val expires: Int = expiresValue
+    @JsonProperty("scopes") val scopes: String = scopesValue
     @JsonProperty("refresh_token") val refreshToken: String? = refreshTokenValue
 }
--- auth/webapp/src/main/kotlin/uk/co/grahamcox/worldbuilder/auth/webapp/oauth2/ClientCredentialsController.kt
+++ auth/webapp/src/main/kotlin/uk/co/grahamcox/worldbuilder/auth/webapp/oauth2/ClientCredentialsController.kt
@@ -3,7 +3,9 @@ package uk.co.grahamcox.worldbuilder.auth.webapp.oauth2
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.ResponseBody
+import uk.co.grahamcox.worldbuilder.auth.oauth2.Scopes
 
 /**
  * Controller for issuing Client Credentials Grant tokens
@@ -18,5 +20,9 @@ class ClientCredentialsController {
     @RequestMapping(value = "/token",
             params = arrayOf("grant_type=client_credentials"))
     @ResponseBody
-    fun token() = AccessTokenResponse("abcdef", "Bearer", 3600)
+    fun token(@RequestParam(value = "scopes", required = false, defaultValue = "*") scopes: Scopes) =
+            AccessTokenResponse(accessTokenValue = "abcdef",
+                    tokenTypeValue = "Bearer",
+                    expiresValue = 3600,
+                    scopesValue = scopes.toString())
 }
